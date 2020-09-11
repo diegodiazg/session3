a240e4b314b7bcfe5280c85b4173c368de69369b
Developer <admin@diegodiazgt.com>
2020-09-11 02:27:05 -0600

    FTR - Render template.

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,9 +1,12 @@
 # from django.urls import path
 from django.conf.urls import url
+
+
 from . import views
 
 urlpatterns = [
     url(r'^$',
         views.HomeListView.as_view(), name='home-list'),
+  	url(r'^clientes-lista/$', views.lista_clientes, name='lista-clientes')
 
-]
+]	
--- apps/home/views.py
+++ apps/home/views.py
@@ -2,8 +2,15 @@
 
 # Create your views here.
 from django.views.generic import TemplateView
-
+from django.shortcuts import render
+from apps.clientes.models import Cliente
 
 class HomeListView(TemplateView):
 
     template_name = 'home/index.html'
+
+def lista_clientes(request):
+	clientes = Cliente.objects.all()
+	dic = { 'clientes': clientes }
+	
+	return render(request, 'home/lista-clientes.html', dic)
